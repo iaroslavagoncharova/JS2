8f7da4f51faee1f13585a903dd1c12edbc9e41bf
darri <iaroslag@metropolia.fi>
2022-11-15 10:04:22 +0200

    .

--- 4.js
+++ 4.js
@@ -5,5 +5,4 @@ do {
     numbers.push(number);
 } while (number != 0);
 numbers.sort(function(a, b){return b-a});
-let kohde = document.querySelector('#kohde');
-kohde.innerHTML = numbers
+console.log(numbers)
